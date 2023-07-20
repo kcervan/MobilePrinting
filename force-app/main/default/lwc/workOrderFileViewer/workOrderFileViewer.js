50c7f38f52ddaed314e712756209fb0a427f6e17
Kortney Cervantes <kortneycervantes@gmail.com>
2023-07-20 16:40:53 -0600

    commit

--- force-app/main/default/lwc/workOrderFileViewer/workOrderFileViewer.js
+++ force-app/main/default/lwc/workOrderFileViewer/workOrderFileViewer.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
+import WorkOrderFileViewer from 'c/workOrderFileViewer';
 
-export default class WorkOrderFileViewer extends LightningElement {}
+// Mock the lightning-file-card component
+jest.mock(
+    'lightning/fileCard',
+    () => ({
+        __esModule: true,
+        default: jest.fn()
+    }),
+    { virtual: true }
+);
+
+describe('c-work-order-file-viewer', () => {
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        jest.clearAllMocks();
+    });
+
+    it('renders the file viewer', () => {
+        // Arrange
+        const element = createElement('c-work-order-file-viewer', { is: WorkOrderFileViewer });
+        document.body.appendChild(element);
+
+        // Assert
+        expect(element).toBeTruthy();
+    });
+});
